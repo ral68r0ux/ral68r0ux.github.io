75bd17640c0ec7107cf67bc87a417e2dada0223a
ral68r0ux <lar17049@byui.edu>
2021-02-06 09:06:09 -0700

    preston undate part 2

---
+++ lesson05/js/message.js
@@ -0,0 +1,9 @@
+const today = new Date();
+const dayNumber - today.getDay();
+const elemen = document.getElementById("message");
+
+if (dayNumber == 6) {
+  element.classList.add("showme") ;
+} else {
+  element.classList.add("hidewme") ;
+}
