4272ffa253f6a566999d0b0b8d158c7a7425c21b
ral68r0ux <lar17049@byui.edu>
2021-03-05 21:22:07 -0700

    prophets

--- lesson09/js/prophets.js
+++ lesson09/js/prophets.js
@@ -5,17 +5,30 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject); // temporary checke for valid respone and data parsing
+    const prophets = jsonObject['prophets'];
     for (let i = 0; i < prophets.length; i++) {
       let card = document.createElement('section');
       let h2 = document.createElement('h2');
+      h2.classList.add('title');
+      let prophImage = document.createElement('img');
+      let birthDate = document.createElement('p');
+      birthDate.classList.add('birthInfo');
+      let birthPlace = document.createElement('p');
+      birthPlace.classList.add('birthInfo');
 
       h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+      prophImage.setAttribute('src', prophets[i].imageurl);
+      prophImage.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname);
+      birthDate.textContent = 'Date of Birth: ' + prophets[i].birthdate;
+      birthPlace.textContent = 'Place of Birth: ' + prophets[i].birthplace;
 
       card.appendChild(h2);
+      card.appendChild(birthDate);
+      card.appendChild(birthPlace);
+      card.appendChild(prophImage);
 
-      document.querySelector('div.cards').appendChild(card);
+      document.querySelector('div.cards').appendChild(card); 
     }
   });
 
-const prophets = jsonObject['prophets'];
+
