807bd699e58df8967439951dc419baf276fc19a1
ral68r0ux <lar17049@byui.edu>
2021-04-04 12:51:34 -0600

    final10

--- noogdenchamer/js/directory.js
+++ noogdenchamer/js/directory.js
@@ -6,7 +6,7 @@ fetch(requestURL)
   .then(function (membersObject) {
 
     const members = membersObject['members'];
-    
+console.log(members); 
     
     for (let i = 0; i < members.length; i++) {
 
@@ -15,6 +15,10 @@ fetch(requestURL)
 
         let info = document.createElement('section');
         info.setAttribute('class', 'infoBlock');
+        
+        let logo = document.createElement('picture');
+        logo.setAttribute('class', 'logoBlock');
+
         let photo = document.createElement('picture');
         photo.setAttribute('class', 'photoBlock');
 
@@ -24,18 +28,28 @@ fetch(requestURL)
         let cityLine = document.createElement('p');
         let phoneNumber = document.createElement('p');
         let domain = document.createElement('p');
+        let photoimg = document.createElement('img');
 
         logoImage.setAttribute('src', members[i].logo);
         logoImage.setAttribute('alt', 'Logo');
         logoImage.setAttribute('width', '150');
         logoImage.setAttribute('height', 'auto');
+        
+        photoimg.setAttribute('src', members[i].imageurl);
+        photoimg.setAttribute('alt', 'Store Image');
+        photoimg.setAttribute('width', '300');
+        photoimg.setAttribute('height', 'auto');
+        
         h3.textContent = members[i].business;
         street.textContent= members[i].address;
         cityLine.textContent= members[i].city + " " + members[i].state + ", " + members[i].zipcode;
         phoneNumber.textContent = members[i].phonenumber;
         domain.textContent = members[i].webaddress;
 
-        photo.appendChild(logoImage);
+        logo.appendChild(logoImage);
+        photo.appendChild(photoimg);
+        
+        card.appendChild(logo);
         card.appendChild(photo);
 
         info.appendChild(h3);
